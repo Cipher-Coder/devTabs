c3c6ef6cd503adbcd0b83191b2e31f80f78729d7
Cipher-Coder <matthew.rebehn@gmail.com>
2019-02-19 16:00:51 -0500

    added clock

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,77 @@ GithubFeed.init({
     console.log("Feed Loaded");
   }
 });
+
+let tday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let tmonth = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+function GetClock() {
+  let d = new Date();
+  let nday = d.getDay(),
+    nmonth = d.getMonth(),
+    ndate = d.getDate(),
+    nyear = d.getFullYear();
+  let nhour = d.getHours(),
+    nmin = d.getMinutes(),
+    nsec = d.getSeconds(),
+    ap;
+
+  if (nhour == 0) {
+    ap = " AM";
+    nhour = 12;
+  } else if (nhour < 12) {
+    ap = " PM";
+  } else if (nhour == 12) {
+    ap = " PM";
+  } else if (nhour > 12) {
+    ap = " PM";
+    nhour -= 12;
+  }
+
+  if (nmin <= 9) nmin = "0" + nmin;
+  if (nsec <= 9) nsec = "0" + nsec;
+
+  let clocktext =
+    "" +
+    tday[nday] +
+    ", " +
+    tmonth[nmonth] +
+    " " +
+    ndate +
+    ", " +
+    nyear +
+    " " +
+    "<br/>" +
+    nhour +
+    ":" +
+    nmin +
+    ":" +
+    nsec +
+    ap +
+    "";
+  document.getElementById("clock").innerHTML = clocktext;
+}
+
+GetClock();
+setInterval(GetClock, 1000);
